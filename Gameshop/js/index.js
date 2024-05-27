d3844da23e27e4a1d9beeddb4a8dcf583e0e61f7
Sua0764 <imdnasuecho@gmail.com>
2024-05-27 17:13:21 +0900

    0527

--- Gameshop/js/index.js
+++ Gameshop/js/index.js
@@ -22,6 +22,15 @@ function displayProducts(gameData) {
       img.classList.add("image");
       img.src = data.image;
       game.appendChild(img);
+      const title = document.createElement("p");
+      const genre = document.createElement("p");
+      const price = document.createElement("p");
+      title.textContent = "게임타이틀 : " + data.title;
+      genre.textContent = "게임 장르 : " + data.genre;
+      price.textContent = "게임 가격 : " + data.price + "원";
+      game.appendChild(title);
+      game.appendChild(genre);
+      game.appendChild(price);
       content.appendChild(game);
     });
   }
